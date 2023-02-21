be429afa09586fbb661cd8d9ba92b547d192c7bf
Efrain Santiago <SantiaEfrain@outlook.com>
2023-02-21 14:25:02 -0600

    wirefram weapon and charater classes.

---
+++ Dungeon Library/Character.cs
@@ -0,0 +1,27 @@
+﻿namespace Dungeon_Library
+{
+    public class Character
+    {
+        //Fields
+
+        /*
+         * int maxlife (assign first in ctor)
+         * int life (cannot be more than maxlife)
+         * string name
+         * int hit chance
+         * int block
+         */
+
+
+        //Properties - 1 for each field
+
+        //Constructors - fully qualified, 1 default/unequalified
+
+        //Methods
+        //ToString() override
+
+        //CalcBlock() returns an int -> return Block;
+        //CalcHitChance() returns an int -> returns HitChance;
+        //CalcDamage() returns an int -> return 0;
+    }
+}
---
+++ Dungeon/TestHarness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    internal class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            //Build and test the fuctionality of our library.
+            //Build and test a weapon
+            //Build and test a Character - include CalcBlock(), CalcHitChance(), CalcDamage()
+        }
+    }
+}
